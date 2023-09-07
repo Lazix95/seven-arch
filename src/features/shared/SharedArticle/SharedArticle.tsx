f23a33049def7d01d7ff45f0e919730a0cdbd426
Lazix95 <lazix95@live.com>
2023-09-07 14:38:50 +0200

    Article fade animation added

--- src/features/shared/SharedArticle/SharedArticle.tsx
+++ src/features/shared/SharedArticle/SharedArticle.tsx
@@ -2,9 +2,9 @@ import { SharedFirebaseImage } from '@/features/firebase/components/FirebaseImag
 import { SharedGridContainer } from '../SharedDrawer/SharedGridContainer';
 import { SharedGridItem } from '../grid/SharedGridItem';
 import { SharedHeading } from '../text/SharedHeading';
-import { useMemo } from 'react';
+import { useEffect, useMemo, useRef, useState } from 'react';
 import { SharedIf } from '../util/SharedIf';
-import { Hidden } from '@mui/material';
+import { Fade, Hidden } from '@mui/material';
 import classes from './SharedArticle.module.scss';
 import { Article, SubArticle } from '@/models/articleModels';
 import { SharedCardSubscribeToNewsTeller } from '@/features/shared/cards/SharedCardSubscribeToNewsTeller';
@@ -18,63 +18,100 @@ export interface SharedArticleProps {
   readonly onSubscribe?: (email: string) => void;
 }
 
-export const SharedArticleChildrenNames = {
-  description: 'description',
-} as const;
-
 export function SharedArticle({ article, onSubscribe, onArticleCLick, onSubArticleClick }: SharedArticleProps) {
+  const [isShown, setIsShown] = useState(false);
   const imageWidth = useMemo(() => {
     // && (!article.subArticles || article.subArticles.length === 0)
     if (article && article.size == 'large' && !article.feature) return 12;
     return 8;
   }, [article]);
 
+  const targetRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    const options = {
+      root: null, // Use the viewport as the root
+      rootMargin: '0px', // No margin
+      threshold: 0, // Trigger when 50% of the element is visible
+    };
+
+    const callback: IntersectionObserverCallback = (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          setIsShown(true);
+          console.log('in the view port');
+          // Element is in the viewport
+          console.log('Element is visible');
+          // Perform your desired actions here
+        }
+      });
+    };
+
+    const observer = new IntersectionObserver(callback, options);
+
+    if (targetRef.current) {
+      observer.observe(targetRef.current);
+    }
+
+    return () => {
+      if (targetRef.current) {
+        observer.unobserve(targetRef.current);
+      }
+    };
+  }, []);
+
   return (
     <SharedIf If={!!article}>
-      <SharedGridContainer spacing={2} centerX={false}>
-        <SharedGridItem xs={12}>
-          <SharedHeading fontWeight={500} color={'white'} level={4}>
-            {article?.title || ''}
-          </SharedHeading>
-        </SharedGridItem>
-
-        <SharedGridItem xs={12} sm={6} md={imageWidth} xl={imageWidth < 12 ? 9 : 12} className={classes['sharedArticle__height']}>
-          <SharedFirebaseImage url={article.imageExternalUrl ?? article.image?.url} text={article?.content} onClick={() => onArticleCLick?.(article)} />
-        </SharedGridItem>
-
-        <Hidden mdDown>
-          <SharedIf If={!!article?.feature && article.feature.type === 'newsTeller'}>
-            <SharedGridItem xs={6} sm={4} xl={3} className={clsx(classes.sharedArticle__height)}>
-              <SharedCardSubscribeToNewsTeller
-                containerProps={{ className: `u-overflow__auto` }}
-                text={article?.feature?.content || ''}
-                btnText={'Subscribe'}
-                align={article?.feature?.align}
-                fontSize={article.feature?.fontSize}
-                onSubscribe={onSubscribe}
-              />
-            </SharedGridItem>
-          </SharedIf>
-        </Hidden>
-
-        <Hidden mdDown>
-          <SharedIf If={!!article?.feature && article.feature.type === 'description'}>
-            <SharedGridItem xs={6} sm={4} xl={3} className={`${classes.sharedArticle__height} u-overflow__auto`}>
-              <SharedCardDescription text={article?.feature?.content || ''} align={article?.feature?.align} fontSize={article.feature?.fontSize} />
-            </SharedGridItem>
-          </SharedIf>
-        </Hidden>
-
-        <Hidden smDown>
-          <SharedIf If={!!article?.subArticles && article.subArticles.length > 0}>
-            {article?.subArticles?.map((subArticle) => (
-              <SharedGridItem key={subArticle.id} xs={6} sm={6} md={4} xl={3} className={classes.sharedArticle__height}>
-                <SharedFirebaseImage url={subArticle.imageExternalUrl ?? subArticle.image?.url} text={subArticle.content} onClick={() => onSubArticleClick?.(subArticle)} />
+      <div ref={targetRef}>
+        <Fade in={isShown} timeout={1000}>
+          <div>
+            <SharedGridContainer spacing={2} centerX={false}>
+              <SharedGridItem xs={12}>
+                <SharedHeading fontWeight={500} color={'white'} level={4}>
+                  {article?.title || ''}
+                </SharedHeading>
               </SharedGridItem>
-            ))}
-          </SharedIf>
-        </Hidden>
-      </SharedGridContainer>
+
+              <SharedGridItem xs={12} sm={6} md={imageWidth} xl={imageWidth < 12 ? 9 : 12} className={classes['sharedArticle__height']}>
+                <SharedFirebaseImage url={article.imageExternalUrl ?? article.image?.url} text={article?.content} onClick={() => onArticleCLick?.(article)} />
+              </SharedGridItem>
+
+              <Hidden mdDown>
+                <SharedIf If={!!article?.feature && article.feature.type === 'newsTeller'}>
+                  <SharedGridItem xs={6} sm={4} xl={3} className={clsx(classes.sharedArticle__height)}>
+                    <SharedCardSubscribeToNewsTeller
+                      containerProps={{ className: `u-overflow__auto` }}
+                      text={article?.feature?.content || ''}
+                      btnText={'Subscribe'}
+                      align={article?.feature?.align}
+                      fontSize={article.feature?.fontSize}
+                      onSubscribe={onSubscribe}
+                    />
+                  </SharedGridItem>
+                </SharedIf>
+              </Hidden>
+
+              <Hidden mdDown>
+                <SharedIf If={!!article?.feature && article.feature.type === 'description'}>
+                  <SharedGridItem xs={6} sm={4} xl={3} className={`${classes.sharedArticle__height} u-overflow__auto`}>
+                    <SharedCardDescription text={article?.feature?.content || ''} align={article?.feature?.align} fontSize={article.feature?.fontSize} />
+                  </SharedGridItem>
+                </SharedIf>
+              </Hidden>
+
+              <Hidden smDown>
+                <SharedIf If={!!article?.subArticles && article.subArticles.length > 0}>
+                  {article?.subArticles?.map((subArticle) => (
+                    <SharedGridItem key={subArticle.id} xs={6} sm={6} md={4} xl={3} className={classes.sharedArticle__height}>
+                      <SharedFirebaseImage url={subArticle.imageExternalUrl ?? subArticle.image?.url} text={subArticle.content} onClick={() => onSubArticleClick?.(subArticle)} />
+                    </SharedGridItem>
+                  ))}
+                </SharedIf>
+              </Hidden>
+            </SharedGridContainer>
+          </div>
+        </Fade>
+      </div>
     </SharedIf>
   );
 }
